530954cf5ae516f03eeb01f9902e2429911c0d7c
yven <yvenxx@163.com>
2025-08-16 01:56:25 +0800

    feat: 配置Element Plus中文语言包

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
 import * as ElementPlusIconsVue from '@element-plus/icons-vue';
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
 const app = createApp(App);
 
@@ -11,5 +12,7 @@ for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component);
 }
 
-app.use(ElementPlus);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
 app.mount("#app");
